a027f93946b0328a8ef46fa662d54c097da4a498
Anton Lukashevich <anton.lukashevich@itechart-group.com>
2018-11-23 15:28:23 +0300

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Fib from "./Fib";
+import OtherPage from "./OtherPage";
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <Link to={'/'}>Home</Link>
+            <Link to={'/otherPage'}>Other page</Link>
+            <div>
+              <Route exact path={'/'} component={Fib}/>
+              <Route exact path={'/otherPage'} component={OtherPage}/>
+            </div>
+          </header>
+        </div>
+      </Router>
+
+    );
+  }
+}
+
+export default App;
